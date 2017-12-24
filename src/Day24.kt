dfeb3948b0b52f44ea0830c7cbfee4c897359f05
Francesco Colasuonno <fcolasuonno@gmail.com>
2017-12-24 16:30:20 +0000

    A bit faster

--- src/Day24.kt
+++ src/Day24.kt
@@ -3,8 +3,6 @@ object Day24 {
     data class Component(val start: Int, val end: Int) {
         val value = start + end
 
-        fun connections() = setOf(start, end)
-
         override fun toString(): String {
             return "$start,$end"
         }
@@ -12,13 +10,13 @@ object Day24 {
 
     fun part1(input: List<String>): Int {
         val components = input.map { it.split('/').map { it.toInt() } }.map { Component(it[0], it[1]) }
-        return components.filter { it.connections().contains(0) }.map {
-            combinations(it.connections().single { it != 0 }, components.minus(it), listOf(it)).map { it.sumBy { it.value } }.max() ?: 0
+        return components.filter { it.start == 0 || it.end == 0 }.map {
+            combinations(if (it.start != 0) it.start else it.end, components.minus(it), listOf(it)).map { it.sumBy { it.value } }.max() ?: 0
         }.max() ?: 0
     }
 
     private fun combinations(previous: Int, usableComponents: List<Component>, initialBridge: List<Component>): List<List<Component>> {
-        return mutableListOf(initialBridge) + usableComponents.filter { it.connections().contains(previous) }.flatMap { component ->
+        return mutableListOf(initialBridge) + usableComponents.filter { it.start == previous || it.end == previous }.flatMap { component ->
             val continuation = if (component.start == previous) component.end else component.start
             combinations(continuation, usableComponents.minus(component), initialBridge + component)
         }
@@ -26,8 +24,8 @@ object Day24 {
 
     fun part2(input: List<String>): Int {
         val components = input.map { it.split('/').map { it.toInt() } }.map { Component(it[0], it[1]) }
-        val longestBridges = components.filter { it.connections().contains(0) }.flatMap {
-            combinations(it.connections().single { it != 0 }, components.minus(it), listOf(it))
+        val longestBridges = components.filter { it.start == 0 || it.end == 0 }.flatMap {
+            combinations(if (it.start != 0) it.start else it.end, components.minus(it), listOf(it))
         }.groupBy { it.size }.maxBy { it.key }?.value
         return longestBridges.orEmpty().map { it.sumBy { it.value } }.max() ?: 0
     }
